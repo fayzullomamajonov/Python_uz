fd95e8ec9318a5d19ae84e1bbbf2b43dffabd65d
fayzullomamajonov <153016957+fayzullomamajonov@users.noreply.github.com>
2024-04-07 23:55:04 +0500

    change setting

--- config/settings.py
+++ config/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL')),
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env('DATABASE_URL')),
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
