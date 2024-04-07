1eb7c19f1849efd13f6412256b8a7beb11defd4f
Shahlo Safarova <152991623+ShahloSafarova@users.noreply.github.com>
2024-04-07 01:44:32 -0700

    statics in settings va urls

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +118,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 
+
+MEDIA_URL = "images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIAFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/media/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = False
+
+AUTH_USER_MODEL = "registration_app.CustomUser"
