0b13071f0e864dad8f354c7ac48d837b52684231
Nurjahon001 <144453104+Nurjahon001@users.noreply.github.com>
2024-04-07 12:37:05 +0300

    change2

---
+++ course_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import (
+    homepage,
+    # runcode,
+    # index,
+)
+
+urlpatterns = [
+    path("", homepage, name="homepage"),
+    # path("home/<int:pk>/", homepage, name="homepage_with_pk"),
+    # path("index", index, name="index"),
+    # path("runcode/", runcode, name="runcode"),
+]
