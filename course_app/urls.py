cec2d2727aa05343d913e09166527d1691d09b9e
Nurjahon001 <144453104+Nurjahon001@users.noreply.github.com>
2024-04-07 12:41:44 +0300

    change

--- course_app/urls.py
+++ course_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import (
-    homepage,
+from .views import homepage
     # runcode,
     # index,
-)
+
 
 urlpatterns = [
     path("", homepage, name="homepage"),
